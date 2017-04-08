095ebdccee681b44bf9c22d459105b7fe34fd362
bryan1027 <b1000074@gmail.com>
2017-04-08 14:51:28 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,10 @@
 var BIS = document.createElement("img");
 BIS.src="images/map.png"
 var canvas = document.getElementById("game-canvas");
-var ctx = canvas.getContext("2d")
+var ctx = canvas.getContext("2d");
+
+function draw(){
+ ctx.drawImage(BIS,0,0) 
+}
+
+draw();
