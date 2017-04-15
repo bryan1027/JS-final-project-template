e275ff3288e065aec24b88845d291b02fbf5a305
bryan1027 <b1000074@gmail.com>
2017-04-15 01:49:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,13 @@
 var bgImg = document.createElement("img");
 var enemyImg = document.createElement("img");
 var towerBtn = document.createElement("img");
+var towerImg = document.createElement("img");
+
 bgImg.src="images/map.png"
 enemyImg.src="images/rukia.gif"
 towerBtn.src="images/tower-btn.png"
+towerImg.src="images/tower.png"
+
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
@@ -14,10 +18,21 @@ var enemy ={
  y:448
 }
 
+var cursur = {
+ x:0,
+ y:0
+}
+
+$("#game-canvas").on("mousemove",function(event){
+ cursur.x=event.offsetX
+ cursur.y=event.offsetY
+})
+
 function draw(){
  ctx.drawImage(bgImg,0,0) 
  ctx.drawImage(enemyImg,enemy.x,enemy.y) 
  ctx.drawImage(towerBtn,560,432,48,48)
+ ctx.drawImage(towerImg,cursur.x,cursur.y)
 }
 
 setInterval(draw,16);
