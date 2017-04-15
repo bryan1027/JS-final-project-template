d4d82d57b0ddfd24fd9b1951cb91926cd3042de7
bryan1027 <b1000074@gmail.com>
2017-04-15 01:04:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ var enemy ={
 function draw(){
  ctx.drawImage(bgImg,0,0) 
  ctx.drawImage(enemyImg,enemy.x,enemy.y) 
- ctx.drawImage(towerBtn,560,432)
+ ctx.drawImage(towerBtn,560,432,30,40)
 }
 
 setInterval(draw,16);
